28ab258d0fb0045c6fc1cf421f0f3ee070d112b7
IsraelLohuan <srlohuan@gmail.com>
2022-08-20 10:33:57 -0300

    Adicionado campo de pesquisa e widget Badge para contar os itens do carrinho

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,89 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class HomeTab extends StatelessWidget {
+
+  const HomeTab({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: const TextStyle(
+              fontSize: 30
+            ),
+            children: [
+              TextSpan(
+                text: 'Green', 
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor
+                )
+              ),
+              TextSpan(
+                text: 'grocer', 
+                style: TextStyle(
+                  color: CustomColors.customConstratColor
+                )
+              )
+            ]
+          )
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15
+            ),
+            child: GestureDetector(
+              onTap: () => print('...'),
+              child: Badge(
+                badgeColor: CustomColors.customConstratColor,
+                badgeContent: const Text('2', style: TextStyle(color: Colors.white, fontSize: 12)),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                prefixIcon: Icon(Icons.search, color: CustomColors.customConstratColor, size: 21,),
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none
+                  )
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
