09bc86d42b4ec7e0a72f0b3f025fddf6ec4edace
IsraelLohuan <srlohuan@gmail.com>
2022-08-20 11:05:25 -0300

    Adicionado categorias

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -2,9 +2,26 @@ import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
-class HomeTab extends StatelessWidget {
+import 'components/category_tile.dart';
+
+class HomeTab extends StatefulWidget {
 
   const HomeTab({ Key? key }) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  final List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais'
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -56,33 +73,57 @@ class HomeTab extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10
-            ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                isDense: true,
-                hintText: 'Pesquise aqui...',
-                prefixIcon: Icon(Icons.search, color: CustomColors.customConstratColor, size: 21,),
-                hintStyle: TextStyle(
-                  color: Colors.grey.shade400,
-                  fontSize: 14
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(60),
-                  borderSide: const BorderSide(
-                    width: 0,
-                    style: BorderStyle.none
-                  )
-                )
-              ),
+          buildSearch(),
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });  
+                  },
+                  category: categories[index], 
+                  isSelected: categories[index] == selectedCategory
+                );
+              }, 
+              separatorBuilder: (_, index) => const SizedBox(width: 10,), 
+              itemCount: categories.length
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Padding buildSearch() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          isDense: true,
+          hintText: 'Pesquise aqui...',
+          prefixIcon: Icon(Icons.search, color: CustomColors.customConstratColor, size: 21,),
+          hintStyle: TextStyle(
+            color: Colors.grey.shade400,
+            fontSize: 14
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(60),
+              borderSide: const BorderSide(
+                width: 0,
+                style: BorderStyle.none
+            )
+          )
+        ),
       ),
     );
   }
