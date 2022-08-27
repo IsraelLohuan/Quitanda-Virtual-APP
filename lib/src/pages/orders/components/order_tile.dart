dac169940f2d3fd070261ee2160c1431ed77b795
IsraelLohuan <srlohuan@gmail.com>
2022-08-27 12:31:31 -0300

    Adicionado botão de PIX

--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -35,6 +35,7 @@ class OrderTile extends StatelessWidget {
             ],
           ),
           childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             IntrinsicHeight(
               child: Row(
@@ -61,6 +62,37 @@ class OrderTile extends StatelessWidget {
                     )
                   )
                 ],
+              ),
+            ),
+
+            Text.rich(
+              TextSpan(
+                style: const TextStyle(
+                  fontSize: 20
+                ),
+                children: [
+                  const TextSpan(
+                    text: 'Total ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  TextSpan(text: UtilsServices.priceToCurrecy(order.total))
+                ]
+              )
+            ),
+
+            Visibility(
+              visible: order.status == 'pending_payment',
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+                ),
+                onPressed: () {}, 
+                icon: Image.asset('assets/app_images/pix.png', height: 10,), 
+                label: const Text('Ver QR Code Pix')
               ),
             )
           ],
