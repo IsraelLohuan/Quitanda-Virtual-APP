92b0cd837db0e2b830f887e7f28a47451313fc79
IsraelLohuan <srlohuan@gmail.com>
2022-08-28 15:32:48 -0300

    Adicionado Shimmer

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -3,6 +3,7 @@ import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
 import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 import '../../config/app_data.dart' as appData;
 import '../common_widgets/app_name_widget.dart';
@@ -24,6 +25,17 @@ class _HomeTabState extends State<HomeTab> {
 
   void itemSelectedCartAnimations(GlobalKey gkImage) {
     runAddToCardAnimation(gkImage);
+  }
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() => isLoading = false);
+    });
   }
 
   @override
@@ -69,7 +81,7 @@ class _HomeTabState extends State<HomeTab> {
             buildSearch(),
             buildCategories(),
             Expanded(
-              child: GridView.builder(
+              child: !isLoading ? GridView.builder(
                 padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
                 physics: const BouncingScrollPhysics(),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -85,6 +97,18 @@ class _HomeTabState extends State<HomeTab> {
                     cartAnimationMethod: itemSelectedCartAnimations
                   );
                 }
+              ): GridView.count(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                  physics: const BouncingScrollPhysics(),
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10, 
+                  childAspectRatio:  9 / 11.5,
+                  children: List.generate(10, (_) => CustomShimmer(
+                    height: double.infinity,
+                    width: double.infinity,
+                    borderRadius: BorderRadius.circular(20),
+                )),
               ),
             )
           ],
@@ -97,7 +121,7 @@ class _HomeTabState extends State<HomeTab> {
     return Container(
       padding: const EdgeInsets.only(left: 25),
       height: 40,
-      child: ListView.separated(
+      child: !isLoading ? ListView.separated(
         scrollDirection: Axis.horizontal,
         itemBuilder: (_, index) {
           return CategoryTile(
@@ -112,6 +136,18 @@ class _HomeTabState extends State<HomeTab> {
         }, 
         separatorBuilder: (_, index) => const SizedBox(width: 10,), 
         itemCount: appData.categories.length
+      ) : ListView(
+        scrollDirection: Axis.horizontal,
+        children: List.generate(5, (_) =>  Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(right: 12),
+            child: CustomShimmer(
+              height: 20,
+              width: 80,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        )
       ),
     );
   }
