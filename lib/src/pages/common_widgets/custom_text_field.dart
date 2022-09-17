d500472024ebc958bffe3c46a5f3c62e04265bbb
IsraelLohuan <srlohuan@gmail.com>
2022-09-17 16:08:04 -0300

    Adicionado validators

--- lib/src/pages/common_widgets/custom_text_field.dart
+++ lib/src/pages/common_widgets/custom_text_field.dart
@@ -10,6 +10,7 @@ class CustomTextField extends StatefulWidget {
   final List<TextInputFormatter>? inputFormatters;
   final String? Function(String?)? validator;
   final TextEditingController? controller;
+  final TextInputType? textInputType;
 
   const CustomTextField({ 
     Key? key, 
@@ -20,7 +21,8 @@ class CustomTextField extends StatefulWidget {
     this.initialValue,
     this.readOnly = false,
     this.validator,
-    this.controller
+    this.controller,
+    this.textInputType
   }) : super(key: key);
 
   @override
@@ -47,6 +49,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
         inputFormatters: widget.inputFormatters,
         obscureText: isObscure,
         validator: widget.validator,
+        keyboardType: widget.textInputType,
         decoration: InputDecoration(
           suffixIcon: Visibility(
             visible: widget.isSecret,
