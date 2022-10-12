f6fb4eb60ef610a3509cdb7a33b81b541250a590
IsraelLohuan <srlohuan@gmail.com>
2022-10-12 10:40:12 -0300

    Finalizado busca de categoria via API

--- lib/src/pages/home/controller/home_controller.dart
+++ lib/src/pages/home/controller/home_controller.dart
@@ -1,8 +1,52 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/pages/home/repository/home_repository.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
+
+import '../result/home_result.dart';
 
 class HomeController extends GetxController {
 
-  void printExample() {
-    print('Funcionou');
+  final homeRepository = HomeRepository();
+
+  bool isLoading = false;
+  List<CategoryModel> allCategories = [];
+  CategoryModel? currentCategory;
+
+  @override 
+  void onInit() {
+    super.onInit();
+    getAllCategories();
+  }
+
+  void selectCategory(CategoryModel category) {
+    currentCategory = category;
+    update();
+  }
+
+  void setLoading(bool value) {
+    isLoading = value;
+    update();
+  }
+
+  Future<void> getAllCategories() async {
+    setLoading(true);
+
+    HomeResult<CategoryModel> homeResult = await homeRepository.getAllCategories();
+
+    setLoading(false);
+
+    homeResult.when(
+      sucess: (data) {
+        allCategories.assignAll(data);
+
+        if(allCategories.isEmpty) return;
+
+        selectCategory(allCategories.first);
+      }, 
+      error: (message) {
+        UtilsServices.showToast(message: message, isError: true);
+      }
+    );
   }
 }
--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,19 +1,22 @@
 import 'package:greengrocer/src/constants/endpoint.dart';
+import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class HomeRepository {
   final HttpManager _httpManager = HttpManager();
 
-  getAllCategories() async {
+  Future<HomeResult<CategoryModel>> getAllCategories() async {
     final result = await _httpManager.restRequest(
       url: Endpoint.getAllCategories, 
       method: HttpMethods.post,
     );
 
     if(result['result'] != null) {
-
+      List<CategoryModel> data = List<Map<String, dynamic>>.from(result['result']).map(CategoryModel.fromJson).toList();
+      return HomeResult<CategoryModel>.sucess(data);
     } else {
-      
+      return HomeResult.error('Ocorreu um erro inesperado ao recuperar as categorias!');
     }
   }
 }
